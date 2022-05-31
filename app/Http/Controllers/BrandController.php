2cfa3a53da7707b91accf60782d6d258336f9e59
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2022-05-30 23:28:47 -0300

    295. Injetando a instância do Model no Controller e como lidar com o type Hinting

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -7,39 +7,50 @@ use Illuminate\Http\Request;
 
 class BrandController extends Controller
 {
+    public function __construct(Brand $brand) {
+        $this->brand = $brand;
+    }
+
     public function index()
     {
-        $brands = Brand::all();
-
+        // $brands = Brand::all();
+        $brands = $this->brand->all();
         return $brands;
     }
 
     public function store(Request $request)
     {
-        $brand = Brand::create($request->all());
+        // $brand = Brand::create($request->all());
+        $brand = $this->brand->create($request->all());
         return $brand;
     }
 
-    public function show(Brand $brand)
+    // public function show(Brand $brand)
+    public function show($id)
     {
+        $brand = $this->brand->find($id);
         return $brand;
     }
 
-    public function update(Request $request, Brand $brand)
+    public function update(Request $request, $id)
     {
         // print_r($request->all()); // dado satualizados
         // echo '<hr>';
         // print_r($brand->getAttributes()); /// dados antigos
-
         // PUT tem o intuito semantico de atualizar todo conteudo //PATCH intuito semantico de atualizar parte do conteudo
+        // $brand->update($request->all());
 
+        $brand = $this->brand->find($id);
         $brand->update($request->all());
         return $brand;
     }
 
-    public function destroy(Brand $brand)
+    public function destroy($id)
     {
         // print_r($brand->getAttributes());
+        // $brand->delete();
+
+        $brand = $this->brand->find($id);
         $brand->delete();
         return ['msg' => 'A marca foi removida com sucesso!'];
     }
