f1c8d9e26a8deb6c8bf31d4fedc6afca555f4c6c
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2022-06-01 15:36:49 -0300

    321. Repository Design Pattern - Implementando um AbstractRepository para modelo

---
+++ app/Repositories/AbstractRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+abstract class AbstractRepository { // pesquisa dinâmica
+
+    public function __construct(Model $model) {
+        $this->model = $model;
+    }
+
+    public function selectAttributesRelatedRecords($attribut) {
+        $this->model = $this->model->with($attribut); // sempre atualiza o proprio atributo
+        // query está sendo montada
+    }
+
+    public function filt($filts) {
+        $filts = explode(';', $filts); // divide o grupo de comparações
+        foreach ($filts as $key => $condition) {
+
+            $c = explode(':', $condition);
+            $this->model = $this->model->where($c[0], $c[1], $c[2]);
+            // a query está sendo montada
+        }
+    }
+
+    public function selectAttribut($attribut) {
+        $this->model = $this->model->selectRaw($attribut);
+        // query está sendo montada
+    }
+
+    public function getResult() {
+        return $this->model->get();
+    }
+
+}
--- app/Repositories/BrandRepository.php
+++ app/Repositories/BrandRepository.php
@@ -2,43 +2,9 @@
 
 namespace App\Repositories;
 
-use Illuminate\Database\Eloquent\Model;
-
-class BrandRepository {
-
-    public function __construct(Model $model) {
-        $this->model = $model;
-    }
-
-    public function selectAttributesRelatedRecords($attribut) {
-        $this->model = $this->model->with($attribut); // sempre atualiza o proprio atributo
-        // query está sendo montada
-    }
-
-    public function filt($filts) {
-        $filts = explode(';', $filts); // divide o grupo de comparações
-        foreach ($filts as $key => $condition) {
-
-            $c = explode(':', $condition);
-            $this->model = $this->model->where($c[0], $c[1], $c[2]);
-            // a query está sendo montada
-        }
-    }
-
-    public function selectAttribut($attribut) {
-        $this->model = $this->model->selectRaw($attribut);
-        // query está sendo montada
-    }
-
-    public function getResult() {
-        return $this->model->get();
-    }
+class BrandRepository extends AbstractRepository {
 
 }
-
-
-
-
 
 
 
