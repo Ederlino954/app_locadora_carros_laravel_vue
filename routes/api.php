5237197edeb65d3b2970d31b84ecd9ddf932de73
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2022-06-02 02:41:43 -0300

    329. Criando as rotas de autenticação e autorização

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,8 @@ Route::apiResource('car', 'App\Http\Controllers\CarController');
 Route::apiResource('rent', 'App\Http\Controllers\RentController');
 Route::apiResource('brand', 'App\Http\Controllers\BrandController');
 Route::apiResource('type', 'App\Http\Controllers\TypeController');
+
+Route::post('/login', 'App\Http\Controllers\AuthController@login');
+Route::post('/logout', 'App\Http\Controllers\AuthController@logout');
+Route::post('/refresh', 'App\Http\Controllers\AuthController@refresh');
+Route::post('/me', 'App\Http\Controllers\AuthController@me');
