e23d80410268c22e1090d66ea5b7dd41f6e36fbf
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2022-06-02 01:05:16 -0300

    324. API WebService Rest para o Resource Locacoes

--- app/Http/Controllers/RentController.php
+++ app/Http/Controllers/RentController.php
@@ -3,33 +3,103 @@
 namespace App\Http\Controllers;
 
 use App\Models\Rent;
-use App\Http\Requests\StoreRentRequest;
-use App\Http\Requests\UpdateRentRequest;
+use App\Repositories\RentRepository;
+use Illuminate\Http\Request;
 
 class RentController extends Controller
 {
-    public function index()
-    {
-        //
+    public function __construct(Rent $rent) {
+        $this->rent = $rent;
     }
 
-    public function store(StoreRentRequest $request)
+    public function index(Request $request)
     {
-        //
+        $rentRepository = new RentRepository($this->rent);
+
+        if ($request->has('filt')) {
+            $rentRepository->filt($request->filt);
+        }
+
+        if($request->has('attribut')) {
+            $rentRepository->selectAttribut($request->attribut);
+        }
+
+        return response()->json($rentRepository->getResult(), 200);
     }
 
-    public function show(Rent $rent)
+    public function store(Request $request)
     {
-        //
+        // Accept application/json para validações funcionarem nas APIs
+        $request->validate($this->rent->rules(), $this->rent->feedback());
+
+        $rent = $this->rent->create([
+            'client_id' => $request->client_id,
+            'car_id' => $request->car_id,
+            'period_start_date' => $request->period_start_date, // inicio
+            'expected_end_date_period' => $request->expected_end_date_period, // data final prevista
+            'end_date_performed_period' => $request->end_date_performed_period, // data final realizada
+            'daily_value' => $request->daily_value,
+            'initial_km' => $request->initial_km,
+            'km_final' => $request->km_final
+        ]);
+
+        return response()->json($rent, 201);
     }
 
-    public function update(UpdateRentRequest $request, Rent $rent)
+    public function show($id)
     {
-        //
+        $rent = $this->rent->find($id); //with com os relacionamentos
+        if($rent === null) {
+            return response()->json(['erro' => 'recurso pesquisado não existe'], 404);
+        }
+        return response()->json($rent, 200);
     }
 
-    public function destroy(Rent $rent)
+    public function update(Request $request, $id)
     {
-        //
+        // para atualizar quando for formdata uliliza-se POST e no front com _method no cabeçalho PUT ou PATCH
+        // PUT tem o intuito semantico de atualizar todo conteudo //PATCH intuito semantico de atualizar parte do conteudo
+        $rent = $this->rent->find($id);
+
+        if ($rent === null) {
+            return response()->json(['erro' => 'Não foi possível realizar a solicitação, o recurso solicitado não existe'], 404);
+        }
+
+        if($request->method() === 'PATCH') {
+
+            $dynamicRules = array();
+
+            //  percorrendo todas as regras definidas no model
+            foreach ($rent->rules() as $input => $rule) {
+
+                // coletar apenas as regras aplicáveis aos paramentros parciais da reuisição
+                if(array_key_exists($input, $request->all())) {
+                    $dynamicRules[$input] = $rule;
+                }
+            }
+
+            $request->validate($dynamicRules, $rent->feedback());
+
+        } else {
+            $request->validate($rent->rules(), $rent->feedback());
+        }
+
+        // preencher o objetop rent com os dados do request
+        $rent->fill($request->all());
+        $rent->save();
+
+        return response()->json($rent, 200);
+    }
+
+    public function destroy($id)
+    {
+        $rent = $this->rent->find($id);
+
+        if ($rent === null) {
+            return response()->json(['erro' => 'Não foi possível realizar a exclusão, o recurso solicitado não existe'], 404);
+        }
+
+        $rent->delete();
+        return response()->json(['msg' => 'A locação foi removida com sucesso!'], 200);
     }
 }
--- app/Models/Rent.php
+++ app/Models/Rent.php
@@ -8,4 +8,43 @@ use Illuminate\Database\Eloquent\Model;
 class Rent extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'client_id', 'car_id','period_start_date', 'expected_end_date_period',
+        'end_date_performed_period','daily_value','initial_km', 'km_final'
+    ];
+
+    public function rules()
+    {
+        return [
+            'client_id' => 'exists:clients,id',
+            'car_id' => 'exists:cars,id',
+            'period_start_date' => 'required|date',
+            'expected_end_date_period' => 'required|date',
+            'end_date_performed_period' => 'required|date',
+            'daily_value' => 'required',
+            'initial_km' => 'required|integer',
+            'km_final' => 'required|integer'
+        ];
+    }
+
+    public function feedback()
+    {
+        return [
+            'client_id.exists' => 'O campo cliente não existe',
+            'car_id.exists' => 'O campo carro não existe',
+            'period_start_date.required' => 'O campo data inicial é obrigatório',
+            'expected_end_date_period.required' => 'O campo data final é obrigatório',
+            'end_date_performed_period.required' => 'O campo data final realizada é obrigatório',
+            'initial_km.required' => 'O campo km inicial é obrigatório',
+            'km_final.required' => 'O campo km final é obrigatório',
+            'initial_km.integer' => 'O campo km inicial deve ser um número inteiro',
+            'km_final.integer' => 'O campo km final deve ser um número inteiro',
+            'period_start_date.date' => 'O campo data inicial deve ser uma data válida',
+            'expected_end_date_period.date' => 'O campo data final deve ser uma data válida',
+            'end_date_performed_period.date' => 'O campo data final realizada deve ser uma data válida',
+            'daily_value.required' => 'O campo valor diário é obrigatório'
+        ];
+    }
+    
 }
