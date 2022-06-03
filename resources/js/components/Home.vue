bcca6af77b577daa1128a90ba8bae6dc2a4be8e1
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2022-06-03 00:52:01 -0300

    346. Convertendo a view home em um componente Vue

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card shadow-lg p-3 mb-5 bg-body rounded">
+                    <div class="card-header shadow-sm p-3 mb-5 bg-body rounded">Bem-vindo!</div>
+
+                    <div class="card-body ">
+                        <p>Apresetar indicadores do sistema!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+</script>
