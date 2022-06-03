5e8ce67c83d2dcb929e74452b8ec81412a64b476
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2022-06-03 02:08:26 -0300

    349. Criando o card de busca de registros

--- public/js/app.js
+++ public/js/app.js
@@ -2078,6 +2078,25 @@ module.exports = {
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 /***/ }),
 
@@ -37981,18 +38000,84 @@ var staticRenderFns = [
             "div",
             { staticClass: "card shadow-lg p-3 mb-5 bg-body rounded" },
             [
-              _c(
-                "div",
-                {
-                  staticClass: "card-header shadow-sm p-3 mb-5 bg-body rounded",
-                },
-                [_vm._v("Marcas")]
-              ),
+              _c("div", { staticClass: "card-header " }, [
+                _vm._v("Busca de Marcas"),
+              ]),
               _vm._v(" "),
-              _c("div", { staticClass: "card-body " }, [
-                _c("p", [
-                  _vm._v("Vamos manipualr os registros de marcas por aqui"),
+              _c("div", { staticClass: "card-body" }, [
+                _c("div", { staticClass: "form-row" }, [
+                  _c("div", { staticClass: "col mb-3" }, [
+                    _c(
+                      "label",
+                      { staticClass: "form-label", attrs: { for: "inputId" } },
+                      [_vm._v("ID")]
+                    ),
+                    _vm._v(" "),
+                    _c("input", {
+                      staticClass: "form-control",
+                      attrs: {
+                        type: "number",
+                        id: "inputId",
+                        "aria-describedby": "idHelp",
+                        "aria-placeholder": "ID",
+                      },
+                    }),
+                    _vm._v(" "),
+                    _c(
+                      "div",
+                      {
+                        staticClass: "form-text text-muted",
+                        attrs: { id: "idHelp" },
+                      },
+                      [_vm._v("Opcional. Informe o id do registro")]
+                    ),
+                  ]),
+                  _vm._v(" "),
+                  _c("div", { staticClass: "col mb-3" }, [
+                    _c(
+                      "label",
+                      {
+                        staticClass: "form-label",
+                        attrs: { for: "inputNome" },
+                      },
+                      [_vm._v("Nome")]
+                    ),
+                    _vm._v(" "),
+                    _c("input", {
+                      staticClass: "form-control",
+                      attrs: {
+                        type: "text",
+                        id: "inputId",
+                        "aria-describedby": "nomeHelp",
+                        "aria-placeholder": "Nome da marca",
+                      },
+                    }),
+                    _vm._v(" "),
+                    _c(
+                      "div",
+                      {
+                        staticClass: "form-text text-muted",
+                        attrs: { id: "nomeHelp" },
+                      },
+                      [_vm._v("Opcional. Informe o nome da marca")]
+                    ),
+                  ]),
                 ]),
+                _vm._v(" "),
+                _c(
+                  "div",
+                  { staticClass: "card shadow p-2 mb-2 bg-body rounded" },
+                  [
+                    _c(
+                      "button",
+                      {
+                        staticClass: "btn btn-primary btn-sm ",
+                        attrs: { type: "submit" },
+                      },
+                      [_vm._v("Pesquisar")]
+                    ),
+                  ]
+                ),
               ]),
             ]
           ),
--- resources/js/components/Brands.vue
+++ resources/js/components/Brands.vue
@@ -4,11 +4,30 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card shadow-lg p-3 mb-5 bg-body rounded">
-                    <div class="card-header shadow-sm p-3 mb-5 bg-body rounded">Marcas</div>
+                    <div class="card-header ">Busca de Marcas</div>
 
-                    <div class="card-body ">
-                        <p>Vamos manipualr os registros de marcas por aqui</p>
+                    <div class="card-body">
+
+                        <div class="form-row">
+                            <div class="col mb-3">
+                                <label for="inputId" class="form-label">ID</label>
+                                <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" aria-placeholder="ID">
+                                <div id="idHelp" class="form-text text-muted">Opcional. Informe o id do registro</div>
+                            </div>
+                            <div class="col mb-3">
+                                <label for="inputNome" class="form-label">Nome</label>
+                                <input type="text" class="form-control" id="inputId" aria-describedby="nomeHelp" aria-placeholder="Nome da marca">
+                                <div id="nomeHelp" class="form-text text-muted">Opcional. Informe o nome da marca</div>
+                            </div>
+                        </div>
+
+                        <div class="card shadow p-2 mb-2 bg-body rounded">
+                            <button type="submit" class="btn btn-primary btn-sm ">Pesquisar</button>
+                        </div>
+
                     </div>
+
+
                 </div>
             </div>
         </div>
